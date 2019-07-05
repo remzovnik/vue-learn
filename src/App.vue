54f11b9d55032f0b8bfe42949d4b77a2df432e3a
remzov <remzovnik@gmail.com>
2019-07-05 15:02:57 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,86 @@
 <template>
     <div id="app">
-        <first-component v-show="showFirst" :handleClick="handleClick"></first-component>
-        <second-component v-show="!showFirst" :handleClick="handleClick"></second-component>
+        <div class="slider">
+            <div class="slider__wrapper">
+                <Slide
+                    v-for="(slide, index) in slides"
+                    v-if="index === currentSlide"
+                    :src="slide.src"
+                    :key="index"
+                />
+                <button class="slider__nav-leftside" type="button" @click="slideBack()">&lt;</button>
+                <button class="slider__nav-rightside" type="button" @click="slideForward()">&gt;</button>
+            </div>
+        </div>
+        <Paginator
+            :slidesCount="slides.length"
+            :clickHandler="toggleSlide"
+            :currentSlide="currentSlide"
+        />
     </div>
 </template>
 
 <script>
-import FirstComponent from './components/FirstComponent.vue';
-import SecondComponent from './components/SecondComponent.vue';
+import Slide from './components/Slide.vue';
+import Paginator from './components/Paginator.vue';
 
 export default {
-  name: 'app',
-  components: {
-    FirstComponent,
-    SecondComponent,
-  },
-  data() {
-    return {
-      showFirst: true,
-    };
-  },
-  methods: {
-    handleClick(val) {
-      this.showFirst = val;
-    },
-  },
+	name: 'app',
+	components: {
+		Slide,
+		Paginator,
+	},
+	data() {
+		return {
+			currentSlide: 0,
+			slides: [
+				{
+					src: require('@/assets/test-1.jpg'),
+				},
+				{
+					src: require('@/assets/test-2.jpg'),
+				},
+				{
+					src: require('@/assets/test-3.jpg'),
+				},
+			],
+		};
+	},
+	methods: {
+		toggleSlide(index) {
+			this.currentSlide = index;
+		},
+		slideBack() {
+			this.currentSlide === 0
+				? (this.currentSlide = this.slides.length - 1)
+				: --this.currentSlide;
+		},
+		slideForward() {
+			this.currentSlide === this.slides.length - 1
+				? (this.currentSlide = 0)
+				: ++this.currentSlide;
+		},
+	},
 };
 </script>
 
-<style>
-.component > * {
-  margin: 20px;
-}
+<style lang="sass">
+	.slider__wrapper
+		position: relative
+		width: 500px
+		height: 400px
 
-button {
-  font-size: 22px;
-}
+	.slider__nav-leftside
+		position: absolute
+		top: 50%
+		left: 10px
+		tranform: translateY(-50%)
+		width: 30px
+		height: 30px
+
+	.slide-enter
+		opacity: 1
+	.slide-leave
+		opacity: 0
 </style>
  
---
+++ src/components/Slide.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="slide">
+        <img class="slide__img" :src="src" alt />
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'Slide',
+	props: {
+		index: Number,
+		src: String,
+	},
+};
+</script>
+
+<style lang="sass">
+	.slide
+		width: 100%
+		height: 100%
+	.slide__img
+		width: 100%
+		height: 100%	
+		object-fit: cover
+		user-select: none
+		pointer-events: none
+</style>
