7723233b2ec0fdb3fbaa37e7187244148c529c77
remzov <remzovnik@gmail.com>
2019-07-05 15:46:09 +0300

    reseting main

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,9 @@
 <template>
-    <div id="app">
-        <first-component v-show="showFirst" :handleClick="handleClick"></first-component>
-        <second-component v-show="!showFirst" :handleClick="handleClick"></second-component>
-    </div>
+    <div id="app">Main</div>
 </template>
 
 <script>
-import FirstComponent from './components/FirstComponent.vue';
-import SecondComponent from './components/SecondComponent.vue';
-
 export default {
 	name: 'app',
-	components: {
-		FirstComponent,
-		SecondComponent,
-	},
-	data() {
-		return {
-			showFirst: true,
-		};
-	},
-	methods: {
-		handleClick(val) {
-			this.showFirst = val;
-		},
-	},
 };
 </script>
-
-<style>
-.component > * {
-	margin: 20px;
-}
-
-button {
-	font-size: 22px;
-}
-</style>
- 
