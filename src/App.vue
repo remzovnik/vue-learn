849c80428e5b33e92be30a8ff19596feada558b8
remzov <remzovnik@gmail.com>
2019-07-20 15:49:57 +0300

    slot coding

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-    <div id="app">Main</div>
+    <div id="app">
+        <div class="container">
+            <layout>
+                <template #header="slotProps">{{slotProps.user.name}}</template>
+                <template>Main text</template>
+                <template #footer>Footer text</template>
+            </layout>
+        </div>
+    </div>
 </template>
 
 <script>
+import BaseLayout from '@/components/BaseLayout.vue';
 export default {
 	name: 'app',
+	components: {
+		layout: BaseLayout,
+	},
+	data() {
+		return {
+			headerData: {
+				logo: 'logo',
+				menu: 'About Catalog Contacts',
+			},
+		};
+	},
 };
 </script>
+
+<style>
+.container {
+	max-width: 1200px;
+	margin: 0 auto;
+	padding: 0 10px;
+}
+</style>
